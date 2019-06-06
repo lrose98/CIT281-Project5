76a8760dd9ff5a0d1773b6434cf77ce3f935ba12
lrose <lrose@uoregon.edu>
2019-06-05 22:23:09 -0700

    Add App-1

---
+++ learn-react/src/App-1.js
@@ -0,0 +1,7 @@
+// jshint esversion: 6
+
+import React from "react";
+import HelloWorld1 from "./HelloWorld1";
+import "./App.css";
+
+export default HelloWorld1;
---
+++ learn-react/src/index-1.js
@@ -0,0 +1,22 @@
+// jshint esversion: 6
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld1 from "./HelloWorld1";
+import * as serviceWorker from "./serviceWorker";
+
+//optional: add style rules to index.css, and uncomment the following line
+//import "./index.css";
+
+var destination = document.querySelector("#root");
+
+//create an instance of a component, and add to DOM
+ReactDOM.render(
+  // call the component
+  <HelloWorld1 />,
+  destination
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
